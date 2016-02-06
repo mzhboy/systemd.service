bb010b75a6239640a3c54e8da971b445184afad0
Mr.槑 <mzhboy@gmail.com>
2016-02-06 19:52:13 +0800

    add aria2c.service, tested on debian sid

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 
 ln -sf /lib/systemd/system/thunder.service /etc/systemd/system/multi-user.target.wants/
 ln -sf /lib/systemd/system/shadowsocks.service /etc/systemd/system/multi-user.target.wants/
+ln -sf /lib/systemd/system/aria2c.service /etc/systemd/system/multi-user.target.wants/
